35ce897700e1ae8ff6e78cabae488a378f30bc5d
r-eiyama <r-eiyama@macloud.jp>
2023-08-14 21:55:48 +0900

    デグレを解消

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,11 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from routers import task, done
 
 app = FastAPI()
+app.include_router(task.router)
+app.include_router(done.router)
 
 
 @app.get("/")
@@ -13,3 +16,5 @@ async def read_main():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
